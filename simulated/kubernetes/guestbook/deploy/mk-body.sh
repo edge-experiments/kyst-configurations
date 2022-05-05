7ff27e4f27fb73105858cedf0cc3676f4ffd5211
Jun Duan <jun.duan@ibm.com>
2022-05-05 17:24:23 -0400

    working setup with mk-body.sh and kustomize

--- simulated/kubernetes/guestbook/deploy/mk-body.sh
+++ simulated/kubernetes/guestbook/deploy/mk-body.sh
@@ -4,5 +4,6 @@ manifest_dir=${1:-"../manifests/"}
 
 for file in $manifest_dir/*; do
     echo -n "  - " >> ./head.yaml
-    cat $file | base64 >> ./head.yaml
+    cat $file | base64 -w 0 >> ./head.yaml
+    echo "" >> ./head.yaml
 done
